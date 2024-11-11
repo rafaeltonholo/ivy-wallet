04d49318c271d7bef042e50041e1dc5fde2d7280
Rafael Tonholo <rafael@tonholo.dev>
2024-11-11 12:31:35 -0400

    refactor: code cleanup

--- shared/base/src/main/java/com/ivy/base/legacy/FileUtil.kt
+++ shared/base/src/main/java/com/ivy/base/legacy/FileUtil.kt
@@ -3,44 +3,8 @@ package com.ivy.base.legacy
 import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
-import android.os.Environment
 import android.provider.OpenableColumns
 import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-
-@Deprecated("useless")
-fun saveFile(
-    context: Context,
-    directoryType: String,
-    fileName: String,
-    content: String
-) {
-    val dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-        ?: return
-    val newFile = File("$dirPath/$fileName")
-    newFile.createNewFile()
-    newFile.writeText(content)
-}
-
-fun writeToFile(context: Context, uri: Uri, content: String) {
-    try {
-        val contentResolver = context.contentResolver
-
-        contentResolver.openFileDescriptor(uri, "w")?.use {
-            FileOutputStream(it.fileDescriptor).use { fOut ->
-                val writer = fOut.writer(charset = Charsets.UTF_16)
-                writer.write(content)
-                writer.close()
-            }
-        }
-    } catch (e: FileNotFoundException) {
-        e.printStackTrace()
-    } catch (e: IOException) {
-        e.printStackTrace()
-    }
-}
 
 fun Context.getFileName(uri: Uri): String? = when (uri.scheme) {
     ContentResolver.SCHEME_CONTENT -> getContentFileName(uri)
